464cb0b8a3af230c20ba14bfd20fe6ed92b16e04
avtechnologiesit <98891888+avtechnologiesit@users.noreply.github.com>
2026-06-18 14:20:49 +0530

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { domains: ['replicate.delivery', 'pbxt.replicate.delivery'] }
+};
+module.exports = nextConfig;
